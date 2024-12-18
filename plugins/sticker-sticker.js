2c11ab92a5058cd326f0f212802aa31732432ce6
Elrebelde1 <sebastianbarbaro82@gmail.com>
2024-12-17 23:37:15 -0400

    Actualizar sticker-sticker.js

--- plugins/sticker-sticker.js
+++ plugins/sticker-sticker.js
@@ -60,7 +60,7 @@ hours = (hours < 10) ? "0" + hours : hours
 minutes = (minutes < 10) ? "0" + minutes : minutes
 seconds = (seconds < 10) ? "0" + seconds : seconds
 
-return minutes + " m y " + seconds + " s " 
+return minutes + " m y " + seconds + "  " 
 }
 
 const isUrl = (text) => {
