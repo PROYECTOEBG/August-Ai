3b7a48a99906b528d1619bdaf7a8ec9c1454e865
Elrebelde1 <sebastianbarbaro82@gmail.com>
2024-12-23 23:46:47 -0400

    Actualizar group-hidetag.js

--- plugins/group-hidetag.js
+++ plugins/group-hidetag.js
@@ -29,7 +29,7 @@ const handler = async (m, {conn, text, participants, isOwner, isAdmin}) => {
       var mediax = await quoted.download?.();
       conn.sendMessage(m.chat, {sticker: mediax, mentions: users}, {quoted: m});
     } else {
-      await conn.relayMessage(m.chat, {extendedTextMessage: {text: `${masss}\n${htextos}\n`, ...{contextInfo: {mentionedJid: users, externalAdReply: {thumbnail: 'https://i.ibb.co/h959Vjm/file.jpg', sourceUrl: 'https://whatsapp.com/channel/0029Vak6H9l3GJP4FJH5063R'}}}}}, {});
+      await conn.relayMessage(m.chat, {extendedTextMessage: {text: `${masss}\n${htextos}\n`, ...{contextInfo: {mentionedJid: users, externalAdReply: {thumbnail: imagen1, sourceUrl: 'instagram.com/ineffable.mvrco'}}}}}, {});
     }
   }
 };
