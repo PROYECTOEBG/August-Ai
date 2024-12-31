9b9ef9f2bcc4a878005aa209d00b4901743d8a32
Elrebelde1 <sebastianbarbaro82@gmail.com>
2024-12-31 14:41:25 -0400

    Actualizar barbozx-myns.js

--- plugins/barbozx-myns.js
+++ plugins/barbozx-myns.js
@@ -1,20 +1,11 @@
-import {createHash} from 'crypto';
+import { createHash } from 'crypto'
 
-
-const handler = async function(m, {conn, text, usedPrefix}) {
-  const datas = global
-  const idioma = datas.db.data.users[m.sender].language
-  const _translate = JSON.parse(fs.readFileSync(`./language/${idioma}.json`))
-  const tradutor = _translate.plugins.rpg_myns
-
-  const sn = createHash('md5').update(m.sender).digest('hex');
-  m.reply(`┏┅ ━━━━━━━━━━━━ ┅ ━
-┃${tradutor.texto1} 
-┃ ${sn}
-┗┅ ━━━━━━━━━━━━ ┅ ━`.trim());
-};
-handler.help = ['myns'];
-handler.tags = ['xp'];
-handler.command = /^(myns|ceksn)$/i;
-handler.register = true;
-export default handler;
+let handler = async function (m, { conn, text, usedPrefix }) {
+let sn = createHash('md5').update(m.sender).digest('hex')
+await conn.reply(m.chat, `${sn}`, m, rcanal)
+}
+handler.help = ['sn']
+handler.tags = ['rg']
+handler.command = ['nserie', 'sn', 'mysn'] 
+handler.register = true
+export default handler
