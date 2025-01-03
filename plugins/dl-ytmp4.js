b38f239a979f649c53d712f693526070fde74727
Elrebelde1 <sebastianbarbaro82@gmail.com>
2025-01-02 23:57:01 -0400

    Actualizar dl-ytmp4.js

--- plugins/dl-ytmp4.js
+++ plugins/dl-ytmp4.js
@@ -1,24 +1,20 @@
-import Scraper from "@SumiFX/Scraper"
+// *[ ❀ YTMP4 ]*
+import fetch from 'node-fetch';
 
-let handler = async (m, { conn, args, usedPrefix, command }) => {
-if (!args[0]) return m.reply('🍭 Ingresa el enlace del vídeo de YouTube junto al comando.\n\n`Ejemplo:`\n' + `> *${usedPrefix + command}* https://youtu.be/QSvaCSt8ixs`)
-if (!args[0].match(/youtu/gi)) return conn.reply(m.chat, `Verifica que el enlace sea de YouTube.`, m)
+let HS = async (m, { conn, text }) => {
+if (!text) return conn.reply(m.chat, `❀ Ingresa un link de youtube`, m)
 
-let user = global.db.data.users[m.sender]
 try {
-let { title, size, quality, thumbnail, dl_url } = await Scraper.ytmp4(args[0])
-if (size.includes('GB') || size.replace(' MB', '') > 300) { return await m.reply('El archivo pesa mas de 300 MB, se canceló la Descarga.')}
-let txt = `╭─⬣「 *YouTube Download* 」⬣\n`
-    txt += `│  ≡◦ *⭐ Titulo ∙* ${title}\n`
-    txt += `│  ≡◦ *🪴 Calidad ∙* ${quality}\n`
-    txt += `│  ≡◦ *⚖ Peso ∙* ${size}\n`
-    txt += `╰─⬣`
-await conn.sendFile(m.chat, thumbnail, 'thumbnail.jpg', txt, m)
-await conn.sendFile(m.chat, dl_url, title + '.mp4', `*⭐ Titulo ∙* ${title}\n*🪴 Calidad ∙* ${quality}`, m, false, { asDocument: user.useDocument })
-} catch {
+let api = await fetch(`https://restapi.apibotwa.biz.id/api/ytmp4?url=${text}`)
+let json = await api.json()
+let title = json.data.metadata.title
+let dl_url = json.data.download.url
+await conn.sendMessage(m.chat, { video: { url: dl_url }, fileName: `${json.data.filename}.mp4`, mimetype: "video/mp4" }, { quoted: m })
+
+} catch (error) {
+console.error(error)
 }}
-handler.help = ['ytmp4 <yt url>']
-handler.tags = ['downloader']
-handler.command = ['ytmp4', 'yt', 'ytv'] 
-//handler.limit = 1
-export default handler
+
+HS.command = ['ytmp4']
+
+export default HS
