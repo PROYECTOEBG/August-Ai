c6351da9b50b8483bc75e3c8fd171e109ba971d2
Elrebelde1 <sebastianbarbaro82@gmail.com>
2025-01-02 23:39:57 -0400

    Actualizar descargas-ytmp3.js

--- plugins/descargas-ytmp3.js
+++ plugins/descargas-ytmp3.js
@@ -1,35 +1,67 @@
-import fetch from 'node-fetch'
+import yts from 'yt-search';
+const handler = async (m, { conn, text, usedPrefix, command }) => {
+  if (!text) throw `*Por favor ingresa un texto*\nEjemplo:\n${usedPrefix + command} https://youtu.be/QSvaCSt8ixs`;
 
-let handler = async (m, { conn, text }) => {
-  if (!text) {
-    return m.reply("❀ Por favor, ingresa una URL válida de YouTube.")
-  }
-    await m.react('🕓')
+  const isVideo = /vid|2|mp4|v$/.test(command);
+  const search = await yts(text);
 
-  let ytUrlRegex = /^(https?:\/\/)?(www\.)?(youtube\.com|youtu\.be)\/.+$/;
-  if (!ytUrlRegex.test(text)) {
-    return m.reply("❀ La URL ingresada no es válida. Asegúrate de que sea un enlace de YouTube.")
+  if (!search.all || search.all.length === 0) {
+    throw "No se encontraron resultados para tu búsqueda.";
   }
 
+  const videoInfo = search.all[0];
+  const body = `*Youtube By CrowBot*
+
+    • *Título :* » ${videoInfo.title}
+    • *Views :* » ${videoInfo.views}
+    • *Duration :* » ${videoInfo.timestamp}
+    • *Uploaded :* » ${videoInfo.ago}
+    • *URL :* » ${videoInfo.url}
+
+> ${espera}`;
+
+m.react(rwait)
+  conn.sendMessage(m.chat, {
+    image: { url: videoInfo.thumbnail },
+    caption: body,
+  }, { quoted: fkontak });
+
+m.react(done)
+  let result;
   try {
-    let api = await fetch(`https://api.giftedtech.my.id/api/download/dlmp3?apikey=gifted&url=${text}`)
-    let json = await api.json()
-    let { quality, title, download_url } = json.result
+    if (command === 'play' || command === 'yta' || command === 'ytmp3') {
+      let hh = await fetch(`https://api.siputzx.my.id/api/dl/youtube/mp3?url=${videoInfo.url}`);
+      result = await hh.json()
+    } else if (command === 'playvid' || command === 'ytv' || command === 'play2' || command === 'ytmp4') {
+    let rr = await fetch(`https://deliriussapi-oficial.vercel.app/download/ytmp4?url=${videoInfo.url}`);
+      result = await rr.json()
+    } else {
+      throw "Comando no reconocido.";
+    }
+let url_dl = isVideo ? result.data.download.url : result.data
+    conn.sendMessage(m.chat, {
+      [isVideo ? 'video' : 'audio']: { url: url_dl },
+      mimetype: isVideo ? "video/mp4" : "audio/mpeg",
+      caption: isVideo ? `URL: ${videoInfo.url}` : '',
+    }, { quoted: m });
 
-    await m.react('✅')
-    await conn.sendMessage(m.chat, { 
-      audio: { url: download_url }, 
-      fileName: `${title}.mp3`, 
-      mimetype: 'audio/mp4' 
-    }, { quoted: m })
   } catch (error) {
-    console.error(error)
-    m.reply("❀ Hubo un error al procesar la URL. Inténtalo nuevamente.")
+m.react('✖️')
+    throw "${errorm2}";
   }
-}
+};
 
-handler.help = ['ytmp3 *<link yt>*']
-handler.tags = ['dl']
-handler.command = ['ytmp3', 'yta', 'fgmp3']
+handler.command = handler.help = ['yta', 'ytmp3', 'Ytmp3'];
+handler.tags = ['descargas'];
+handler.estrellas = 4;
 
-export default handler
+export default handler;
+
+const getVideoId = (url) => {
+  const regex = /(?:v=|\/)([0-9A-Za-z_-]{11}).*/;
+  const match = url.match(regex);
+  if (match) {
+    return match[1];
+  }
+  throw new Error("Invalid YouTube URL");
+};
