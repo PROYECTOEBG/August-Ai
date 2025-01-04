867cca4148105ea7f92262edba58911f57d42e48
Elrebelde1 <sebastianbarbaro82@gmail.com>
2025-01-04 01:40:15 -0400

    Actualizar descargas-play.js

--- plugins/descargas-play.js
+++ plugins/descargas-play.js
@@ -4,8 +4,8 @@ import qs from 'qs';
 
 let handler = async (m, { conn, text, usedPrefix, command }) => {
   if (!text) {
-    return m.reply(`Ejemplo de uso: *${usedPrefix + command} Rojo - 27
-By Barboza🔥*`);
+    return m.reply(`Ejemplo de uso: ${usedPrefix + command} _Rojo_ - _27_
+By Barboza🔥`);
   }
 
   const appleMusic = {
