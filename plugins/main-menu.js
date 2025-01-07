e0dea13862e5fdf61df189357aaae1332e65596f
Elrebelde1 <sebastianbarbaro82@gmail.com>
2025-01-07 19:16:58 -0400

    Actualizar main-menu.js

--- plugins/main-menu.js
+++ plugins/main-menu.js
@@ -75,7 +75,7 @@ const defaultMenu = {
   header: '╭═════⊹⊱≼「 *%category* 」≽⊰⊹════•',
   body: '┃☁️› ✧  %cmd %islimit %isPremium\n',
   footer: '╰═════⊹⊱≼≽⊰⊹═════╯',
-  after: `🎄 ${dev}`,
+  after: `⚡ ${dev}`,
 }
 
 let handler = async (m, { conn, usedPrefix: _p, __dirname }) => {
