6669f2197caf7834ac8ff78df46b7c36a7848058
Elrebelde1 <sebastianbarbaro82@gmail.com>
2025-01-07 12:27:00 -0400

    Actualizar handler.js

--- handler.js
+++ handler.js
@@ -414,12 +414,12 @@ conn: this,
 
 global.dfail = (type, m, conn, usedPrefix) => {
     let msg = {
-        rowner: "> _*`Perdon, Este comando es solo para mi Owner☃️.`*_",
-        owner: "> _*`Perdon, Solo mi creador puede usar este comando☃️.`*_",
-        mods: "> _*`Perdon, Este comando solo es para mods☃️`*_",
-        premium: "> _*`No eres un usuario Premium, Habla con mi owner☃️`*_",
-        group: "> _*`Perdon, Este comando solo es para grupos☃️`*_",
-        private: "> _*`Ve a mi chat privado y usa este comando☃️`*_",
+        rowner: "> _*`Perdon, Este comando es solo para mi Owner🚩.`*_",
+        owner: "> _*`Perdon, Solo mi creador puede usar este comando🚩.`*_",
+        mods: "> _*`Perdon, Este comando solo es para mods🚩`*_",
+        premium: "> _*`No eres un usuario Premium, Habla con mi owner🚩`*_",
+        group: "> _*`Perdon, Este comando solo es para grupos🚩`*_",
+        private: "> _*`Ve a mi chat privado y usa este comando🚩`*_",
         admin: "> _*`Quien eres?, tu no eres admin☃️`*_",
         botAdmin: "> _*`Es necesario que sea admin primero para usar esta función☃️`*_",
         unreg: "> _*`‼️USUARIO NO REGISTRADO‼️`*_\n\n`Para Registrarse:`\n\n> .reg nombre.edad\n\n`Ejemplo:`\n\n> .reg Barboza.18",
