277d715043d1f3254c3cf2c26e12972b12f4c49c
Elrebelde1 <sebastianbarbaro82@gmail.com>
2025-01-07 12:32:01 -0400

    Actualizar group-staff.js

--- plugins/group-staff.js
+++ plugins/group-staff.js
@@ -26,7 +26,7 @@ let handler = async (m, { conn }) => {
 
   try {
     // Verificar variables globales con valores predeterminados
-    const imageUrl = global.imageUrl || "https://files.catbox.moe/owl2rl.jpg"; // Imagen predeterminada
+    const imageUrl = global.imageUrl || "https://ibb.co/DCrtVgQ"; // Imagen predeterminada
     const sourceUrl = global.redes || "https://github.com/Deylinel/TECNO-BOT-OFICIAL"; // URL del proyecto
     const thumbnailUrl = global.icono || "https://files.catbox.moe/owl2rl.jpg"; // Miniatura
 
