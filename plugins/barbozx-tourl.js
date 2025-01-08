0698109eba43d95084ffa281948752c653b5b2f1
Elrebelde1 <sebastianbarbaro82@gmail.com>
2025-01-08 16:31:06 -0400

    Actualizar barbozx-tourl.js

--- plugins/barbozx-tourl.js
+++ plugins/barbozx-tourl.js
@@ -1,4 +1,3 @@
-
 import uploadFile from '../lib/uploadFile.js'
 import uploadImage from '../lib/uploadImage.js'
 import fetch from 'node-fetch'
@@ -6,28 +5,35 @@ import fetch from 'node-fetch'
 let handler = async (m) => {
   let q = m.quoted ? m.quoted : m
   let mime = (q.msg || q).mimetype || ''
-  if (!mime) return conn.reply(m.chat, ' Responde a una *Imagen* o *Vídeo.*', m, rcanal)
-  await m.react('🕓')
+  if (!mime) return conn.reply(m.chat, '🐉 Responde a una *Imagen* o *Vídeo.*', m, rcanal)
+  await m.react(rwait)
   try {
+ /* conn.reply(m.chat, global.wait, m, {
+  contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, showAdAttribution: true,
+  title: packname,
+  body: wm,
+  previewType: 0, thumbnail: icons,
+  sourceUrl: channel }}})*/
   let media = await q.download()
   let isTele = /image\/(png|jpe?g|gif)|video\/mp4/.test(mime)
   let link = await (isTele ? uploadImage : uploadFile)(media)
   let img = await (await fetch(`${link}`)).buffer()
-  let txt = '`-  T E L E G R A P H  -  U P L O A D E R`\n\n'
-      txt += `  *🔖 Enlace* : ${link}\n`
-      txt += `  *🔖 Acortado* : ${await shortUrl(link)}\n`
-      txt += `  *🔖 Tamaño* : ${formatBytes(media.length)}\n`
-      txt += `  *🔖 Expiración* : ${isTele ? 'No expira' : 'Desconocido'}\n\n`
-      txt += ` *${textbot}*`
+  let txt = `乂  *L I N K - E N L A C E*  乂\n\n`
+      txt += `*» Enlace* : ${link}\n`
+      txt += `*» Acortado* : ${await shortUrl(link)}\n`
+      txt += `*» Tamaño* : ${formatBytes(media.length)}\n`
+      txt += `*» Expiración* : ${isTele ? 'No expira' : 'Desconocido'}\n\n`
+      txt += `> *${dev}*`
 
-await conn.sendFile(m.chat, img, 'thumbnail.jpg', txt, m, null, rcanal)
-await m.react('✅')
+await conn.sendFile(m.chat, img, 'thumbnail.jpg', txt, m, fkontak, rcanal)
+await m.react(done)
 } catch {
-await m.react('✖️')
+await m.react(error)
 }}
 handler.help = ['tourl']
-handler.tags = ['tools']
-handler.command = /^(tourl|upload)$/i
+handler.tags = ['transformador']
+handler.register = true
+handler.command = ['tourl','upload']
 export default handler
 
 function formatBytes(bytes) {
