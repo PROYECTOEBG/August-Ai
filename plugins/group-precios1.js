9aff213e1bee6ad2462169e4453c4be0aaf461fc
Elrebelde1 <sebastianbarbaro82@gmail.com>
2025-01-11 21:27:02 -0400

    Actualizar group-precios1.js

--- plugins/group-precios1.js
+++ plugins/group-precios1.js
@@ -1,16 +1,24 @@
+
+// CREADOR DARK - CORE  //
+
 let handler = async (m, { conn }) => {
 
     await m.react('💫');
 
-    const message = "AQUI ESTAN LOS PRECIOS.\n\n> 1 semana de spma= 1k de diamantes\n\n> 5 días = 800 diamantes\n\n> 3 días =500 diamantes";
+    const message = `AQUI ESTAN LOS PRECIOS.\n\n> 1 semana de spma = 1k de diamantes\n> 5 días = 800 diamantes\n> 3 días = 500 diamantes`;
+
     if (m.isGroup) {
+        const imageUrl = 'https://d.uguu.se/NWJRyNAU.jpg';
 
-    const imageUrl = 'https://d.uguu.se/NWJRyNAU.jpg'; 
+        await conn.sendMessage(m.chat, { 
+            image: { url: imageUrl }, 
+            caption: message 
+        }, { mimetype: 'image/jpeg' });
+    }
+};
 
-    await conn.sendMessage(m.chat, { image: { url: imageUrl }, caption: message }, { mimetype: 'image/jpeg' });
-    }
-    }
-    handler.help = ['precios1'];
-    handler.tags = ['main'];
-    handler.command = ['precios1', 'p1', 'precio1'];
-    export default handler;
+handler.help = ['precios1'];
+handler.tags = ['main'];
+handler.command = ['precios1', 'p1', 'precio1'];
+
+export default handler;
