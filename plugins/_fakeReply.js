099013a4a0df060e7f9793b9df239a927a235d54
Elrebelde1 <sebastianbarbaro82@gmail.com>
2025-01-11 22:23:31 -0400

    Actualizar _fakeReply.js

--- plugins/_fakeReply.js
+++ plugins/_fakeReply.js
@@ -2,10 +2,10 @@ import fetch from 'node-fetch'
 
 export async function before(m, { conn }) {
 let name = '⚡𝑩𝒐𝒕𝑩𝒂𝒓𝒃𝒐𝒛𝒂 𝑴𝑫 - 𝑪𝒉𝒂𝒏𝒏𝒆𝒍🤖'
-let imagenes = ["https://f.uguu.se/NtXbowVi.jpg",
-"https://f.uguu.se/NtXbowVi.jpg",
-"https://f.uguu.se/NtXbowVi.jpg",
-"https://f.uguu.se/NtXbowVi.jpg"]
+let imagenes = ["https://i.ibb.co/hy3G9Rz/file.jpg",
+"https://i.ibb.co/hy3G9Rz/file.jpg",
+"https://i.ibb.co/hy3G9Rz/file.jpg",
+"https://i.ibb.co/hy3G9Rz/file.jpg"]
 
 let icono = imagenes[Math.floor(Math.random() * imagenes.length)]
 
