18d9adc1180f381db7d61b9b13cae5587b76b5bf
Elrebelde1 <sebastianbarbaro82@gmail.com>
2025-01-20 21:17:54 -0400

    Actualizar rg-reg.js

--- plugins/rg-reg.js
+++ plugins/rg-reg.js
@@ -20,8 +20,8 @@ let handler = async function (m, { conn, text, usedPrefix, command }) {
   user.regTime = + new Date
   user.registered = true
   let sn = createHash('md5').update(m.sender).digest('hex')
-  let img = await (await fetch(`https://qu.ax/llZLr.jpg`)).buffer()
-  let txt = ` –  *Registro - Completado *\n\n`
+  let img = await (await fetch(`https://qu.ax/WVyWh.jpg`)).buffer()
+  let txt = ` –  *Bienvenido A August-Ai*\n\n`
       txt += `╔  👤  *NOMBRE* : ${name}\n`
       txt += `╠  💎  *EDAD* : ${age} años\n`
 await conn.sendAi(m.chat, botname, textbot, txt, img, img, canal, m)
