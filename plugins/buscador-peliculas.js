b8f34c90cdb63510c3769a895f9ff505c7cf3894
Elrebelde1 <sebastianbarbaro82@gmail.com>
2025-01-26 12:51:41 -0400

    Actualizar buscador-peliculas.js

--- plugins/buscador-peliculas.js
+++ plugins/buscador-peliculas.js
@@ -1,64 +1,30 @@
-/* Creado/adaptado por Bruno Sobrino (https://github.com/BrunoSobrino) */
+//Creado por https://github.com/DIEGO-OFC/DORRAT-BOT-MD
 
-import fetch from 'node-fetch';
-import axios from 'axios';
-import {load} from 'cheerio';
-const handler = async (m, {text, usedPrefix, command, conn}) => {
-if (!text) throw`️${lenguajeGB['smsAvisoMG']()}${mid.smsMalused7}\n${usedPrefix + command} El Gato con botas`
-  let aaaa;
-  let img;
-  try {
-    aaaa = await searchC(text);
-    img = 'https://cinefilosoficial.com/wp-content/uploads/2021/07/cuevana.jpg';
-  } catch {
-    aaaa = await searchP(text);
-    img = 'https://elcomercio.pe/resizer/RJM30xnujgfmaODGytH1rRVOrAA=/400x0/smart/filters:format(jpeg):quality(75)/arc-anglerfish-arc2-prod-elcomercio.s3.amazonaws.com/public/BJ2L67XNRRGHTFPKPDOEQ2AH5Y.jpg';
-  }
-  if (aaaa == '') throw `️${lenguajeGB['smsAvisoFG']()}${mid.buscador10}`
-  const res = await aaaa.map((v) => `*🎬 • ${mid.smsYT1}:* ${v.title}\n*🍿 • ${mid.smsYT4}:* ${v.link}`).join`\n\n───────────────\n\n`;
-  const ads = `*💫 • ${mid.buscador11}*\nhttps://block-this.com/block-this-latest.apk\n\n≣≣≣≣≣≣≣≣≣≣≣≣≣≣≣≣≣≣≣≣≣≣≣≣≣≣≣≣≣≣≣≣\n\n`
-  conn.sendMessage(m.chat, {image: {url: img}, caption: ads + res}, {quoted: m});
+import fetch from "node-fetch";
+let handler = async (m, {text, usedPrefix, command, conn}) => {
+  if (!text) throw `*[❕] Ingrese el nombre de una película*\n\n*❍ EJEMPLO: ${usedPrefix + command} Batman*`;
+  let a = await fetch(`https://www.omdbapi.com/?t=${text}&apikey=caba8d6f`);
+  let x = await a.json();
+  let mov_txt = `*${comienzo}「 PELÍCULAS 」${fin}*\n
+*Titulo ∙* ${x.Title || "-"}
+*Publicado ∙* ${x.Year || "-"}
+*Duracion ∙* ${x.Runtime || "-"}
+*Genero ∙* ${x.Genre || "-"}
+*Director ∙* ${x.Director || "-"}
+*Actores ∙* ${x.Actors || "-"}
+*Lenguajes ∙* ${x.Language || "-"}
+*Awards ∙* ${x.Awards || "-"}
+*Votos ∙* ${x.imdbVotes || "-"}
+*Score ∙* ${x.Metascore || "-"}
+*Tipo ∙* ${x.Type || "-"}
+*Recaudado ∙* ${x.BoxOffice || "-"}
+*Grabacion ∙* ${x.Country || "-"}
+
+   ❕Pedido por @${m.sender.split("@")[0]}
+*🔥 THE DORRAT - BOT 🔥*`;
+
+  conn.sendMessage(m.chat, {image: {url: x.Poster}, caption: mov_txt, mentions: [m.sender]}, {quoted: m});
 };
-handler.command = ['cuevana', 'pelisplus'];
-handler.level = 2
-handler.register = true
+handler.command = /^(película|pelicula|peli)$/i;
+handler.limit = true;
 export default handler;
-
-const safeLoad = async (url, options = {}) => {
-  try {
-    const {data: pageData} = await axios.get(url, options);
-    const $ = load(pageData);
-    return $;
-  } catch (err) {
-    if (err.response) {
-      throw new Error(err.response.statusText);
-    }
-    throw err;
-  }
-};
-
-async function searchC(query, numberPage = 1) {
-  const $ = await safeLoad(`https://cuevana3.mu/page/${numberPage}/`, {
-    params: {s: query}});
-  const resultSearch = [];
-  $('.results-post > article').each((_, e) => {
-    const element = $(e);
-    const title = element.find('header > h2').text();
-    const link = element.find('.lnk-blk').attr('href');
-    resultSearch.push({title: title, link: link});
-  });
-  return resultSearch;
-}
-
-async function searchP(query, numberPage = 1) {
-  const $ = await safeLoad(`https://pelisplushd.cx/search/`, {
-    params: {s: query, page: numberPage}});
-  const resultSearch = [];
-  $('a[class^=\'Posters\']').each((_, e) => {
-    const element = $(e);
-    const title = element.find('.listing-content > p').text();
-    const link = element.attr('href');
-    resultSearch.push({title: title, link: link});
-  });
-  return resultSearch;
-}
