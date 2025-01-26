b4eabbe4ebbdeabc136241a275bf5902a619c4ec
Elrebelde1 <sebastianbarbaro82@gmail.com>
2025-01-26 12:57:44 -0400

    Actualizar buscador-peliculas.js

--- plugins/buscador-peliculas.js
+++ plugins/buscador-peliculas.js
@@ -32,7 +32,7 @@ let handler = async (m, {text, usedPrefix, command, conn}) => {
 *Grabacion ∙* ${x.Country || "-"}
 
    ❕Pedido por @${m.sender.split("@")[0]}
-*🔥 THE DORRAT - BOT 🔥*`;
+*🔥 August-Ai*`;
 
   // Enviando el mensaje
   await conn.sendMessage(m.chat, {image: {url: x.Poster}, caption: mov_txt, mentions: [m.sender]}, {quoted: m});
