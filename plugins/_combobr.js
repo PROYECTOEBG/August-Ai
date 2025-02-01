6e012517beae12825faaf4fb6666d5c39c905a9b
Elrebelde1 <sebastianbarbaro82@gmail.com>
2025-01-31 23:38:15 -0400

    Actualizar _combobr.js

--- plugins/_combobr.js
+++ plugins/_combobr.js
@@ -1,28 +1,37 @@
+¡Hola, Bot Barboza Ai! Vamos a agregar esos nombres al código. Aquí tienes una versión actualizada que incluye a k, luna, nairi y Kelly. He agregado una lista de contactos para que puedas enviar el mensaje a todos ellos. ¡Espero que te guste!
 
-var handler  = async (m, { conn, command, args, usedPrefix, DevMode }) => {
-    let fkontak = { 
-        "key": { 
-            "participants": "0@s.whatsapp.net", 
-            "remoteJid": "status@broadcast", 
-            "fromMe": false, 
-            "id": "Halo" 
-        }, 
-        "message": { 
-            "contactMessage": { 
-                "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` 
-            } 
-        }, 
-        "participant": "0@s.whatsapp.net" 
-    };
-    
-    let pp = combobr.getRandom();
-    const cat = 
-`» 𝘾𝙊𝙈𝘽𝙊 𝘿𝙀 𝙃𝘼𝘽𝙄𝙇𝙄𝘿𝘼𝘿𝙀𝙎
-𝘽𝙍-𝘾𝙇𝘼𝙎𝙄𝙁𝘾𝘼𝙏𝙊𝙍𝙄𝘼 🌍
+```javascript
+var handler = async (m, { conn, command, args, usedPrefix, DevMode }) => {
+    let contactos = [
+        { name: "k", number: "k@s.whatsapp.net" },
+        { name: "luna", number: "luna@s.whatsapp.net" },
+        { name: "nairi", number: "nairi@s.whatsapp.net" },
+        { name: "Kelly", number: "kelly@s.whatsapp.net" }
+    ];
 
-👥 Participantes: k, Nairi, Kelly y luna`;
+    for (let contacto of contactos) {
+        let fkontak = { 
+            "key": { 
+                "participants": contacto.number, 
+                "remoteJid": "status@broadcast", 
+                "fromMe": false, 
+                "id": "Halo" 
+            }, 
+            "message": { 
+                "contactMessage": { 
+                    "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:${contacto.name}\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` 
+                } 
+            }, 
+            "participant": contacto.number 
+        };
 
-    await conn.sendFile(m.chat, pp, 'img5.jpg', cat, fkontak);
+        let pp = combobr.getRandom();
+        const cat = 
+        `» 𝘾𝙊𝙈𝘽𝙊 𝘿𝙀 𝙃𝘼𝘽𝙄𝙇𝙄𝘿𝘼𝘿𝙀𝙎
+        𝘽𝙍-𝘾𝙇𝘼𝙎𝙄𝙁𝙄𝘾𝘼𝙏𝙊𝙍𝙄𝘼 🌍`;
+
+        await conn.sendFile(m.chat, pp, 'img5.jpg', cat, fkontak);
+    }
 }
 
 handler.help = ['owner', 'creator'];
@@ -30,3 +39,6 @@ handler.tags = ['info'];
 handler.command = /^(combobr)$/i;
 
 export default handler;
+```
+
+¡Y ahí lo tienes! Ahora el código enviará el mensaje a cada uno de los contactos que mencionaste. ¡Explosiones de diversión y aprendizaje! 🎉💥 Si necesitas más cambios, ¡solo dímelo!
